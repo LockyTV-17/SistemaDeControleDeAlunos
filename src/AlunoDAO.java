0a976cf51bfbb847743a2c96145ff810c8590bd5
LockyTV-17 <luan.lobo.17@gmail.com>
2026-01-28 15:52:06 -0300

    Impediendo vazamento de memoria

--- src/AlunoDAO.java
+++ src/AlunoDAO.java
@@ -1,54 +1,68 @@
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class AlunoDAO {
 
     public void createAluno(String nome, String email) {
-        ConexaoDB conexaoDB = new ConexaoDB();
         int idGerado;
-        ResultSet rs;
+        ResultSet resultadoDosDados = null;
+        PreparedStatement psInsert = null;
         String insertSql = "INSERT INTO alunos (nome, email) VALUES (?,?)";
+        ConexaoDB conexaoDB = new ConexaoDB();
+        Connection connection =  null;
+
 
         //Forma moderna impedindo vazemento de memoria
         //try (Connection conexao = conexaoDB.getConnection();
         //PreparedStatement psInsert = conexao.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS)) {
         try {
-            PreparedStatement psInsert;
-            psInsert = conexaoDB.getConnection().prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
-
+            connection = conexaoDB.getConnection();
+            psInsert = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
             psInsert.setString(1, nome);
             psInsert.setString(2, email);
             psInsert.executeUpdate();
-            rs = psInsert.getGeneratedKeys();
+            resultadoDosDados = psInsert.getGeneratedKeys();
 
             //Forma moderna
             //try(ResultSet rs = psInsert.getGeneratedKeys())    
-            if (rs.next()) {
-                idGerado = rs.getInt(1);
+            if (resultadoDosDados.next()) {
+                idGerado = resultadoDosDados.getInt(1);
                 System.out.printf("INSERT relizado com sucesso \n o Aluno %s foi inserido com o ID %d\n", nome, idGerado);
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Erro no cadastro do aluno" + e.getMessage());// TODO: handle exception
+        } finally{
+            try {
+                if(psInsert != null) psInsert.close();
+                if(resultadoDosDados != null) resultadoDosDados.close();
+                if(connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
     }
 
     public void selecAluno(String nome, String email) {
-        ResultSet resultadoDosDados;
         ConexaoDB conexaoDB = new ConexaoDB();
+        Connection connection = null;
+        ResultSet resultadoDosDados = null;
+        PreparedStatement psSelec = null;
+        String selectSql = "SELECT * FROM alunos WHERE nome = ? AND email = ?";
         try {
-            PreparedStatement psSelec;
-            String selectSql = "SELECT * FROM alunos WHERE nome = ? AND email = ?";
-            psSelec = conexaoDB.getConnection().prepareStatement(selectSql);
+            connection = conexaoDB.getConnection();
+            psSelec = connection.prepareStatement(selectSql);
             psSelec.setString(1, nome);
             psSelec.setString(2, email);
 
-            resultadoDosDados = psSelec.executeQuery();
+             resultadoDosDados= psSelec.executeQuery();
             System.out.println("resultado da Consulta");
 
             if (resultadoDosDados.next()) {
@@ -64,16 +78,27 @@ public class AlunoDAO {
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Erro no achar do aluno" + e.getMessage());
+        }finally{
+            try {
+                if(psSelec !=null) psSelec.close();
+                if(resultadoDosDados !=null) resultadoDosDados.close();
+                if(connection != null)connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
     }
 
     public void updateAluno(String nome, String email, String novoEmail) {
         ConexaoDB conexaoDB = new ConexaoDB();
+        Connection connection = null;
+        PreparedStatement psUpdate = null;
         String upadateSql = "UPDATE alunos SET email = ?  WHERE nome = ? AND email = ?";
         try {
 
-            PreparedStatement psUpdate = conexaoDB.getConnection().prepareStatement(upadateSql);
+            connection = conexaoDB.getConnection();
+            psUpdate = connection.prepareStatement(upadateSql);
             psUpdate.setString(1, novoEmail);
             psUpdate.setString(2, nome);
             psUpdate.setString(3, email);
@@ -83,22 +108,40 @@ public class AlunoDAO {
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Erro em atulizar o aluno" + e.getMessage());
+        }finally {
+            try {
+                if (psUpdate != null) psUpdate.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
     }
 
     public void deleteAluno(String nome, String email) {
         ConexaoDB conexaoDB = new ConexaoDB();
+        PreparedStatement psDelete = null;
         String deleteSql = "DELETE FROM alunos WHERE email = ? AND nome = ?";
+        Connection connection = null;
+        
         try {
-            PreparedStatement psDelete;
-            psDelete = conexaoDB.getConnection().prepareStatement(deleteSql);
+            connection = conexaoDB.getConnection();
+
+            psDelete = connection.prepareStatement(deleteSql);
             psDelete.setString(1, email);
             psDelete.setString(2, nome);
             psDelete.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Erro em Deletar o Aluno" + e.getMessage());
+        } finally {
+            try {
+                if (psDelete != null)  psDelete.close();
+                if (connection != null)  connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
     }
--- src/ConexaoDB.java
+++ src/ConexaoDB.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoDB {
-        public final String STR_CONNECTION = "";
-        public final String USER = "";
-        public final String PASSWORD = "";
+        public final String STR_CONNECTION = "jdbc:mysql://127.0.0.1:3306/sistema_escolar";
+        public final String USER = "root";
+        public final String PASSWORD = "157157";
        
         public  Connection getConnection() throws SQLException{
         return  DriverManager.getConnection(STR_CONNECTION,USER,PASSWORD); 
